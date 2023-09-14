8818c924c82525b4f88454b2ce2e626e478e617a
newbiecoder30 <76021136+newbiecoder30@users.noreply.github.com>
2023-09-13 22:57:47 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Project from './Project';
+
+function App() {
+  const projects = [
+    {
+      title: 'Project 1 test test2 test3',
+      description: 'Description for project 1',
+      link: 'https://github.com/yourusername/project1'
+    },
+    {
+      title: 'Project 2',
+      description: 'Description for project 2',
+      link: 'https://github.com/yourusername/project2'
+    }
+    // ... add more projects as necessary
+  ];
+
+  return (
+    <div>
+      <Header />
+      <section>
+        {projects.map((project, index) => (
+          <Project key={index} {...project} />
+        ))}
+      </section>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Project.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Project({ title, description, link }) {
+  return (
+    <div className="project">
+      <h2>{title}</h2>
+      <p>{description}</p>
+      <a href={link} target="_blank" rel="noopener noreferrer">View Project</a>
+    </div>
+  );
+}
+
+export default Project;
