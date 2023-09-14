3ca18de20f513e8fdeac99c3b93392b20bf7875d
newbiecoder30 <76021136+newbiecoder30@users.noreply.github.com>
2023-09-13 23:26:56 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Project from './Project';
-import Navbar from './Navbar';
+import React from "react";
+import Navbar from "./Navbar";
 
 function App() {
-  const projects = [
-    {
-      title: 'Project 1 test test2 test3',
-      description: 'Description for project 1',
-      link: 'https://github.com/yourusername/project1'
-    },
-    {
-      title: 'Project 2',
-      description: 'Description for project 2',
-      link: 'https://github.com/yourusername/project2'
-    }
-    // ... add more projects as necessary
-  ];
-
   return (
     <div>
       <Navbar></Navbar>
-      <Header />
-      <section>
-        {projects.map((project, index) => (
-          <Project key={index} {...project} />
-        ))}
-      </section>
-      <Footer />
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,9 @@
-import { Link } from 'react-router-dom'
-
-
 const Navbar = () => {
-    return (
-      <nav className="navbar">
-        <h1>Young's Website</h1>
-        <div className="links">
-          <Link to="/">Home</Link>
-          <Link to="/create">New Blog</Link>
-        </div>
-      </nav>
-    );
-}
- 
+  return (
+    <nav className="navbar">
+      <h1>Young's Website</h1>
+    </nav>
+  );
+};
+
 export default Navbar;
